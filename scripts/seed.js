bd0dcd3e79d9b1fbd7616db9781f65e94d9fbc26
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-10-30 22:28:26 +0100

    set up database - postgres

--- scripts/seed.js
+++ scripts/seed.js
@@ -161,8 +161,8 @@ async function seedRevenue() {
 }
 
 (async () => {
-  await seedUsers();
-  await seedCustomers();
-  await seedInvoices();
-  await seedRevenue();
+	await seedUsers();
+	await seedCustomers();
+	await seedInvoices();
+	await seedRevenue();
 })();
